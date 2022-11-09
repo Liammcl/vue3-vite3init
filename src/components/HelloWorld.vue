86a8f236f2cbf900ca7565bd77da9ec466ffabf9
liamzi6666@163.com <liamzi6666@163.com>
2022-11-09 21:14:04 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="father w-[1200px] m-auto">
+    <div class="son text-white">
+      hi,vite+vue3+ts
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.father {
+  // width: 200px;
+  height: 100px;
+  background-color: aquamarine;
+
+  .son {
+    text-align: center;
+    font-size: 20px;
+    line-height: 100px;
+  }
+}
+</style> 
